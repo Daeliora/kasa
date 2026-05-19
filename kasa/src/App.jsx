d1f1454bcc7758dae34afd755e916d0f0dbd0f24
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-05-19 11:52:31 +0200

    Modif route

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -1,7 +1,21 @@
-import Router from './router/Router'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from '../pages/Home/Home'
+import About from '../pages/About/About'
+import Housing from '../pages/Housing/Housing'
+import Error from '../pages/Error/Error'
 
 function App() {
-  return <Router />
+ return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/housing" element={<Housing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App
+export default App;
