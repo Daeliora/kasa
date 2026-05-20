c9417c60e75da708f29cc4a7e7483296fb806a1e
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-05-20 15:37:20 +0200

    Modif main.jsx

--- kasa/src/main.jsx
+++ kasa/src/main.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/Home.jsx'      
+import App from './App.jsx'      
 import About from './pages/About/About.jsx'    
 import Error from './pages/Error/Error.jsx'
 import './styles/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </BrowserRouter>
+      <App />
   </React.StrictMode>
 )
