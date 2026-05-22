ad05f88dc2cf0581e27538e2c4f68affaeed7e3c
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-05-22 16:44:35 +0200

    SCSS pour About

--- kasa/src/pages/About/About.jsx
+++ kasa/src/pages/About/About.jsx
@@ -1,45 +1,49 @@
 import Collapse from '../../components/Collapse.jsx'
+import Banner from '../../components/Banner/Banner'
+import bannerAbout from '../../assets/banner-about.png'
 
 function About() {
   return (
     <main className="about">
+      <Banner image={bannerAbout} />
 
-      <Collapse title="Fiabilité">
-        <p>
-          Les annonces postées sur Kasa garantissent
-          une fiabilité totale. Les photos sont conformes aux logements,
-          et toutes les informations sont régulièrement vérifiées par nos équipes. 
-        </p>
-      </Collapse>
+      <div className="about-collapse">
+        <Collapse title="Fiabilité">
+          <p>
+            Les annonces postées sur Kasa garantissent
+            une fiabilité totale. Les photos sont conformes aux logements,
+            et toutes les informations sont régulièrement vérifiées par nos équipes. 
+          </p>
+        </Collapse>
 
-      <Collapse title="Respect">
-        <p>
-          La bienveillance fait partie des valeurs
-          fondatrices de Kasa. Tout comportement discriminatoire 
-          ou de perturbation du voisinage entraînera une exclusion
-          de notre plateforme.
-        </p>
-      </Collapse>
+        <Collapse title="Respect">
+          <p>
+            La bienveillance fait partie des valeurs
+            fondatrices de Kasa. Tout comportement discriminatoire 
+            ou de perturbation du voisinage entraînera une exclusion
+            de notre plateforme.
+          </p>
+        </Collapse>
 
-      <Collapse title="Service">
-        <p>
-          La qualité du service fait partie des priorités de Kasa. 
-          Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes 
-          ou nos locataires, soit  empreinte de respect et de bienveillance.    
-        </p>
-      </Collapse>
+        <Collapse title="Service">
+          <p>
+            La qualité du service fait partie des priorités de Kasa. 
+            Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes 
+            ou nos locataires, soit  empreinte de respect et de bienveillance.    
+          </p>
+        </Collapse>
 
-      <Collapse title="Sécurité">
-        <p>
-          La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que 
-          pour les voyageurs, chaque logement correspond aux critères de sécurité 
-          établis par nos services. En laissant une note aussi bien à l'hôte 
-          qu'au locataire, cela permet à nos équipes de vérifier que les standards 
-          sont bien respectés. Nous organisons également des ateliers sur la sécurité 
-          domestique pour nos hôtes.
-        </p>
-      </Collapse>
-
+        <Collapse title="Sécurité">
+          <p>
+            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que 
+            pour les voyageurs, chaque logement correspond aux critères de sécurité 
+            établis par nos services. En laissant une note aussi bien à l'hôte 
+            qu'au locataire, cela permet à nos équipes de vérifier que les standards 
+            sont bien respectés. Nous organisons également des ateliers sur la sécurité 
+            domestique pour nos hôtes.
+          </p>
+        </Collapse>
+      </div>
     </main>
   )
 }
